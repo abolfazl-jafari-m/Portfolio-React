e43e44d5993d4e9ae438e36e4ee0726f0c610d20
Abolazl Jafari M <abolfazljafari563@gmail.com>
2025-01-25 22:20:54 +0330

    feat(headline):design headline

--- src/components/Header/HeadLine/HeadLine.jsx
+++ src/components/Header/HeadLine/HeadLine.jsx
@@ -3,6 +3,10 @@ import styles from  "./HeadLine.module.css"
 function HeadLine() {
     return (
         <>
+            <div className={styles.container}>
+                <h1 className={styles.title}>Hello I'm <span>Abolfazl Jafari M</span></h1>
+                <h3 className={styles.subtitle}>Web Developer & Designer</h3>
+            </div>
         </>
     );
 }
