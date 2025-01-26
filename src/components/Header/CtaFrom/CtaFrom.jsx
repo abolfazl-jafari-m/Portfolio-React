9b183e76de887302e031ebfafa125328934b80ad
Abolazl Jafari M <abolfazljafari563@gmail.com>
2025-01-26 11:45:30 +0330

    feat(cta):desgined from

--- src/components/Header/CtaFrom/CtaFrom.jsx
+++ src/components/Header/CtaFrom/CtaFrom.jsx
@@ -2,9 +2,19 @@ import styles from "./CtaForm.module.css"
 
 function CtaFrom() {
     return (
-       <>
-
-       </>
+        <>
+            <form className={styles.form}>
+                <div className={styles.formSection}>
+                    <label htmlFor={"name"}>Your Name :</label>
+                    <input type={"text"} id={"name"} placeholder={"Reza & Ali & etc.."}/>
+                </div>
+                <div className={styles.formSection}>
+                    <label htmlFor={"email"}>Your Email :</label>
+                    <input type={"email"} id={"email"} placeholder={"youremail@mail.con"}/>
+                </div>
+                <button>درخواست همکاری یا ما</button>
+            </form>
+        </>
     );
 }
 
